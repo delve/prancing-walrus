517d4f7fa17f476546510e067817432a0e4808b0
delve <delve202@gmail.com>
2023-11-03 18:10:23 +0000

    wip, nd to get channel name from test

--- walrusbot/botcommands/MyAssignment.go
+++ walrusbot/botcommands/MyAssignment.go
@@ -2,6 +2,7 @@ package botcommands
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/FedorLap2006/disgolf"
 	"github.com/bwmarrin/discordgo"
@@ -19,35 +20,55 @@ var MyAssignment = &disgolf.Command{
 			panic(err)
 		}
 		// fmt.Printf("In Handler err: %v\n", err)
-		// fmt.Printf("In Handler channel: %v\n", thisChan.Name)
+		fmt.Printf("In Handler channel: %v\n", thisChan.Name)
 		if thisChan.Name == "prancingwalrustests" {
+			// get the username
+			name := ctx.Interaction.Member.User.Username
+			// fmt.Printf("In MessageHandler: ctx: %v\n", ctx)
+			// fmt.Printf("In MessageHandler: ctx.interaction: %v\n", ctx.Interaction)
+			// fmt.Printf("In MessageHandler: ctx.interaction.Member: %v\n", ctx.Interaction.Member)
+			// fmt.Printf("In MessageHandler: ctx.interaction.Member.User: %v\n", ctx.Interaction.Member.User)
+			// fmt.Printf("In MessageHandler: ctx.interaction.Member.Nick: %v\n", ctx.Interaction.Member.Nick)
 			_ = ctx.Respond(&discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
-					Content: "Hi meh! You're assigned to the Prospector team and gather fossil type γ",
+					Content: getAssignmentMessage(name),
 				},
 			})
 		} else {
 			_ = ctx.Respond(&discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
-					Content: "This command is only available from the species-war channel.",
+					Content: "This has been a test. Thank you, and please send fish.",
 				},
 			})
 		}
 	}),
 	MessageHandler: disgolf.MessageHandlerFunc(func(ctx *disgolf.MessageCtx) {
-		fmt.Printf("In MessageHandler CTX: %v\n", ctx)
-		_, _ = ctx.Reply("Hi meh! You're assigned to the Prospector team and gather fossil type γ", true)
+		_, _ = ctx.Reply(fmt.Sprintf("In MessageHandler: MessageCtx: %v\n", ctx), true)
 	}),
 
 	// Middlewares array executes (before? after?) a / command handler. because???
 	Middlewares: []disgolf.Handler{
 		disgolf.HandlerFunc(func(ctx *disgolf.Ctx) {
-			fmt.Printf("In Middleware CTX: %v\n", ctx)
+			fmt.Printf("In Middleware Ctx: %v\n", ctx)
 			ctx.Next()
 		}),
 	},
 }
 
-// func getAssignmentMessage(ctx)
+func getAssignmentMessage(name string) (assignMsg string) {
+	assignMsg = "Sorry, looks like I have no assignment for you. Go bug the managers and leave me to my fish."
+	assign, found := assignments[name]
+	if found {
+		var sb strings.Builder
+		sb.WriteString(fmt.Sprintf("Hi %s! You're assigned to the %s kit. Please gather fossil %s.\n", assign.gameName, assign.role, assign.gather))
+		if assign.canUseClamMagic {
+			sb.WriteString("If it's a clam war this week please use spells as you see fit.")
+		} else {
+			sb.WriteString("If it's a clam war this week please use both grow spells, but none of the others.")
+		}
+		assignMsg = sb.String()
+	}
+	return
+}
